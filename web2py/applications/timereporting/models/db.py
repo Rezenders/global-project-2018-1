f5b9730d7f3d3320016853fe5e4adac17685d0b5
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-05-03 19:33:27 -0300

    adding email configs in db.py (change in server)

--- web2py/applications/timereporting/models/db.py
+++ web2py/applications/timereporting/models/db.py
@@ -99,11 +99,9 @@ auth.define_tables(username=False, signature=False)
 # configure email
 # -------------------------------------------------------------------------
 mail = auth.settings.mailer
-mail.settings.server = 'logging' if request.is_local else myconf.get('smtp.server')
-mail.settings.sender = myconf.get('smtp.sender')
-mail.settings.login = myconf.get('smtp.login')
-mail.settings.tls = myconf.get('smtp.tls') or False
-mail.settings.ssl = myconf.get('smtp.ssl') or False
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.sender = 'testidlab@gmail.com'
+mail.settings.login = 'testidlab@gmail.com:123yele123'
 
 # -------------------------------------------------------------------------
 # configure auth policy

0c64bae8e97c1acca7ab05f6b37d2382498fa6ac
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-05-04 10:45:03 -0300

    sending email utilizing send_hours.html as template

--- web2py/applications/timereporting/controllers/email.py
+++ web2py/applications/timereporting/controllers/email.py
@@ -1,5 +1,29 @@
+@auth.requires_membership('manager')
 def send_hours():
-    mail.send(to=['testidlab@gmail.com'],
-            subject='hello',
-            message='Student\'s hours')
-    return "Email sent!"
+    user_id = request.args[0]
+    workshift_id = request.args[1]
+
+    student = db(db.auth_user.id == user_id).select(db.auth_user.first_name)
+
+    workshift = db(db.WorkShift.id == workshift_id).select(db.WorkShift.ALL)
+
+    query = (
+        db.TimeTracker.WorkShift_id == workshift_id) & (
+        db.TimeTracker.user_id == user_id)
+    tracker_status = db(query).select(db.TimeTracker.Approved)
+
+    status = "Rejected"
+    if tracker_status:
+        status = "Approved"
+
+    context = dict(
+        name=student[0].first_name,
+        start=workshift[0].WeekStart,
+        status=status)
+    message = response.render('send_hours.html', context)
+
+    mail_status = mail.send(to=['testidlab@gmail.com'],
+                            subject='DePaul time report',
+                            message=message)
+
+    return redirect(URL('timereporting', 'default', 'index'))

72057c245ef8faff5ead19265190db9936c9324b
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-05-04 11:06:58 -0300

    tracker_id as parameter and reading email from databse

--- web2py/applications/timereporting/controllers/email.py
+++ web2py/applications/timereporting/controllers/email.py
@@ -1,29 +1,33 @@
 @auth.requires_membership('manager')
 def send_hours():
-    user_id = request.args[0]
-    workshift_id = request.args[1]
+    tracker_id = request.args[0]
+    tracker = db(db.TimeTracker.id == tracker_id).select(db.TimeTracker.ALL)
 
-    student = db(db.auth_user.id == user_id).select(db.auth_user.first_name)
+    try:
+        user_id = tracker[0].user_id
+        workshift_id = tracker[0].WorkShift_id
+        tracker_status = tracker[0].Approved
 
-    workshift = db(db.WorkShift.id == workshift_id).select(db.WorkShift.ALL)
+        student = db(db.auth_user.id == user_id).select(db.auth_user.ALL)
+        workshift = db(db.WorkShift.id == workshift_id).select(db.WorkShift.ALL)
 
-    query = (
-        db.TimeTracker.WorkShift_id == workshift_id) & (
-        db.TimeTracker.user_id == user_id)
-    tracker_status = db(query).select(db.TimeTracker.Approved)
+        send_to = student[0].email
 
-    status = "Rejected"
-    if tracker_status:
-        status = "Approved"
+        status = "Rejected"
+        if tracker_status:
+            status = "Approved"
 
-    context = dict(
-        name=student[0].first_name,
-        start=workshift[0].WeekStart,
-        status=status)
-    message = response.render('send_hours.html', context)
+        context = dict(
+                name=student[0].first_name,
+                start=workshift[0].WeekStart,
+                status=status)
+            
+        message = response.render('send_hours.html', context)
 
-    mail_status = mail.send(to=['testidlab@gmail.com'],
-                            subject='DePaul time report',
-                            message=message)
+        mail_status = mail.send(to=[send_to],
+                                subject='DePaul time report',
+                                message=message)
+    except:
+        pass
 
     return redirect(URL('timereporting', 'default', 'index'))

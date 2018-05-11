db9d7814d57b426550f54698db62981e2251e18e
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-05-11 00:27:21 -0300

    typos

--- web2py/applications/timereporting/controllers/email.py
+++ web2py/applications/timereporting/controllers/email.py
@@ -1,26 +1,20 @@
-@auth.requires_membership('manager')
+@auth.requires_membership('Manager')
 def send_hours():
     WorkWeek_id = request.args[0]
     WeekWorked = db(db.WorkWeek.id == WorkWeek_id).select(db.WorkWeek.ALL)
 
     try:
         user_id = WeekWorked[0].user_id
-        #workshift_id = tracker[0].WorkShift_id
-        tracker_status = WorkWeek[0].Approved_Status
+        tracker_status = WeekWorked[0].Approved_Status
 
         student = db(db.auth_user.id == user_id).select(db.auth_user.ALL)
-        #workshift = db(db.WorkShift.id == workshift_id).select(db.WorkShift.ALL)
 
         send_to = student[0].email
 
-        status = "Rejected"
-        if tracker_status:
-            status = "Approved"
-
         context = dict(
                 name=student[0].first_name,
-                start=WorkWeek[0].Monday,
-                status=status)
+                start=WeekWorked[0].Monday,
+                status=tracker_status)
             
         message = response.render('send_hours.html', context)
 

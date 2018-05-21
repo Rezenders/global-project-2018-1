c187c14db991f37b0db94db5c69c335c9f47064b
Gustavo <gustavorezendesilva@hotmail.com>
2018-05-21 16:13:03 -0300

    table names not permitted

--- web2py/applications/depaulapp/models/tablesDef.py
+++ web2py/applications/depaulapp/models/tablesDef.py
@@ -1,4 +1,11 @@
-db.define_table('WorkWeek', Field('Monday','date'),Field('Sunday','date'),Field('Approved_Status',requires = IS_IN_SET(['Needs Approval', 'Rejected','Approved']),default='Needs Approval'),Field('user_id','reference auth_user'),Field('Manager_Comment','string'),Field('Total_Hours','double'))
+db.define_table(
+        'WorkWeek',
+        Field('Monday','date'),
+        Field('Sunday','date'),
+        Field('Approved_Status',requires = IS_IN_SET(['Needs Approval', 'Rejected','Approved']),default='Needs Approval'),
+        Field('user_id','reference auth_user'),
+        Field('Manager_Comment','string'),
+        Field('Total_Hours','double'))
 
 db.define_table(
     'WorkShift',
@@ -8,5 +15,23 @@ db.define_table(
     Field('WorkWeek_id','reference WorkWeek'),
     Field('Last_Changed', 'datetime', default=request.now)
 )
-
-db.define_table('avaliabledh', Field('user_id','reference auth_user', default=auth.user_id, writable=False), Field('monday', 'date', requires = IS_DATE(format=('%Y-%m-%d'))), Field('start_time_on_monday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('end_time_on_monday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('tuesday', 'date', requires = IS_DATE(format=('%Y-%m-%d'))), Field('start_time_on_tuesday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('end_time_on_tuesday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('wednesday', 'date', requires = IS_DATE(format=('%Y-%m-%d'))), Field('start_time_on_wednesday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('end_time_on_wednesday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('thursday', 'date', requires = IS_DATE(format=('%Y-%m-%d'))),Field('start_time_on_thursday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('end_time_on_thursday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('friday', 'date', requires = IS_DATE(format=('%Y-%m-%d'))), Field('start_time_on_friday', 'time', requires = IS_DATE(format=('%H:%M:%S'))), Field('end_time_on_friday', 'time', requires = IS_DATE(format=('%H:%M:%S')))
+db.define_table(
+        'AvaiableDH', 
+        Field('student_name','reference auth_user'), 
+        Field('Monday', 'date'), 
+        Field('StartTimeMonday', 'time'), 
+        Field('EndTimeMonday', 'time'), 
+        Field('Tuesday', 'date'), 
+        Field('StartTimeTuesday', 'time'), 
+        Field('EndTimeTuesday', 'time'),
+        Field('Wednesday', 'date'), 
+        Field('StartTimeWednesday', 'time'), 
+        Field('EndTimeWednesday', 'time'), 
+        Field('Thursday', 'date'),
+        Field('StartTimeThursday', 'time'), 
+        Field('EndTimeThursday', 'time'), 
+        Field('Friday', 'date'), 
+        Field('StartTimeFriday', 'time'), 
+        Field('EndTimeFriday', 'time'), 
+        Field('TotalHoursAvaliable', requires = IS_NOT_EMPTY())
+)

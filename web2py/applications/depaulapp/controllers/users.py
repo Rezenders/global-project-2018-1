f993ae05357e4b55d94ee8f5b95ef9a67b14b203
Gustavo <gustavorezendesilva@hotmail.com>
2018-05-31 00:07:57 -0300

    show_users sqlform load

--- web2py/applications/depaulapp/controllers/users.py
+++ web2py/applications/depaulapp/controllers/users.py
@@ -58,17 +58,75 @@ def show_active():
 
 @auth.requires(auth.has_membership('Managers') or auth.has_membership('Upper Managers'))
 def show_all():
-   users = db().select(db.auth_user.ALL, orderby=db.auth_user.first_name)
-   return dict(users=users)
+    return dict()
+
+@auth.requires(auth.has_membership('Managers') or auth.has_membership('Upper Managers'))
+def show_users():
+
+    export_classes = dict(csv=True, json=False, html=False, tsv=False, xml=False,csv_with_hidden_cols=False,tsv_with_hidden_cols=False)
+    
+    auth_fields = [
+        db.auth_user.first_name,
+        db.auth_user.last_name,
+        db.auth_user.email,
+        db.auth_user.Phone_Number,
+    ]
+
+    links = [
+        dict(header="Role", body = lambda row: role(row)),    
+        dict(header="", body = lambda row: activate_but(row)),    
+        dict(header="", body = lambda row: deactivate_but(row)),    
+    ]
+
+    grid = SQLFORM.smartgrid(
+           db.auth_user,
+           linked_tables =[],
+           fields = dict(auth_user=auth_fields),
+           create = False,
+           editable = False,
+           details = False,
+           deletable = False,
+           exportclasses=dict(auth_user=export_classes),
+           links = links,
+           )
+    return dict(users=grid)
+
+def role(row):
+    user = db(db.auth_user.id == row.id).select().first()
+    ret =''
+    if auth.has_membership(user_id=user.id, role='Students'):
+        ret = 'Student'
+    elif auth.has_membership(user_id=user.id, role='Managers'):
+        ret = 'Manager'
+    elif auth.has_membership(user_id=user.id, role='Upper Managers'):
+        ret = 'Upper Manager'
+    return ret
+
+def deactivate_but(row):
+    user = db(db.auth_user.id == row.id).select().first()
+    upper_manager  = auth.has_membership(user_id=auth.user.id, role='Upper Managers')
+    if user.registration_key =='' and not auth.has_membership(user_id=user.id, role='Upper Managers'):
+        if auth.has_membership(user_id=user.id, role='Managers') and not upper_manager:
+            return ""
+        return BUTTON('Deactivate', _class='btn-sm btn-danger',_onclick="ajax('%s', [], ':eval')" % URL(c='users',f='deactivate_user',args=[user.id]))
+    else:
+        return ""
 
 @auth.requires(auth.has_membership('Managers') or auth.has_membership('Upper Managers'))
 def deactivate_user():
    user_id = request.args[0]
    db.auth_user.update_or_insert(db.auth_user.id == user_id , registration_key='disabled')
-   return redirect(URL('show_all'))
+   return 'web2py_component("%s","users_table");' % URL(c='users',f='show_users.load'); 
+
+def activate_but(row):
+    user = db(db.auth_user.id == row.id).select().first()
+    if user.registration_key !='' and not auth.has_membership(user_id=user.id, role='Upper Managers'):
+        return BUTTON('Activate', _class='btn-sm btn-success',_onclick="ajax('%s', [], ':eval')" % URL(c='users',f='activate_user',args=[user.id]))
+    else:
+        return ""
 
 @auth.requires(auth.has_membership('Managers') or auth.has_membership('Upper Managers'))
 def activate_user():
    user_id = request.args[0]
    db.auth_user.update_or_insert(db.auth_user.id == user_id , registration_key='')
-   return redirect(URL('show_all'))
+   return 'web2py_component("%s","users_table");' % URL(c='users',f='show_users.load');
